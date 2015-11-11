5a46943ae20db9bdb4af9180f0d902f273ad30e9
DaniilPros <dddddddaniil@gmail.com>
2015-11-12 00:16:07 +0200

    Create class of ship

--- Battle_ship/Ship.cpp
+++ Battle_ship/Ship.cpp
@@ -1,14 +1,80 @@
 #include "Ship.h"
 
 
-
-Ship::Ship()
-{
-}
-
-
 Ship::Ship(initializer_list<pair<bool, pair<int, int>>> list)
 {
+	this->parts.insert(this->parts.end(), list.begin(), list.end());
+	this->condition = true;
+}
+
+bool Ship::get_condition()
+{
+	return this->condition;
+}
+
+void Ship::set_condition(bool condition)
+{
+	this->condition = condition;
+}
+
+void Ship::change_condition()
+{
+	this->condition = this->condition ? false : true;
+}
+
+vector<pair<bool, pair<int, int>>> Ship::get_parts()
+{
+	return this->parts;
+}
+
+void Ship::set_parts(vector<pair<bool, pair<int, int>>> parts)
+{
+	this->parts = parts;
+}
+
+pair<bool, pair<int, int>> Ship::get_part(int index_of_part)
+{
+	return parts[index_of_part];
+}
+
+void Ship::set_part(int index_of_part, pair<bool, pair<int, int>> part)
+{
+	this->parts[index_of_part] = part;
+}
+
+void Ship::add_part(pair<bool, pair<int, int>> part)
+{
+	this->parts.push_back(part);
+}
+
+bool Ship::get_condition_of_part(int index_of_part)
+{
+	return this->parts[index_of_part].first;
+}
+
+void Ship::set_condition_of_part(int index_of_part, bool condition)
+{
+	this->parts[index_of_part].first = condition;
+}
+
+void Ship::change_condition_of_part(int index_of_part)
+{
+	this->parts[index_of_part].first = this->parts[index_of_part].first ? false : true;
+}
+
+pair<int, int> Ship::get_position_of_part(int index_of_part)
+{
+	return this->parts[index_of_part].second;
+}
+
+void Ship::set_position_of_part(int index_of_part, pair<int, int> position_of_part)
+{
+	this->parts[index_of_part].second = position_of_part;
+}
+
+int Ship::get_length()
+{
+	return this->parts.size();
 }
 
 Ship::~Ship()
--- Battle_ship/Ship.h
+++ Battle_ship/Ship.h
@@ -3,9 +3,31 @@
 using namespace std;
 class Ship
 {
-	bool condition;
-	vector<pair<bool, pair<int, int>>> coord_of_part;
+	bool condition;//condition of the ship
+	vector<pair<bool, pair<int, int>>> parts;//positions and conditions of parts of the ship
+
 public:
-	Ship(initializer_list<pair<bool,pair<int,int>>> list = {});
-	~Ship();
+	Ship(initializer_list<pair<bool,pair<int,int>>> list = {});//constructor with list of arguments which has pairs of conditions and positions of parts
+
+	bool get_condition();//return condition of the ship
+	void set_condition(bool condition);//set condition of the ship
+	void change_condition();//change condition of the ship on the opposite
+
+	vector<pair<bool, pair<int, int>>> get_parts();//return all the parts of the ship
+	void set_parts(vector<pair<bool, pair<int, int>>> parts);//set parts of the ship
+
+	pair<bool, pair<int, int>> get_part(int index_of_part);//return a part of the ship by index
+	void set_part(int index_of_part, pair<bool, pair<int, int>> part);//set a part of the ship by index
+	void add_part(pair<bool, pair<int, int>> part);//add a part of the ship
+	
+	bool get_condition_of_part(int index_of_part);//return condition of part by index
+	void set_condition_of_part(int index_of_part, bool condition);//set condition of part by index
+	void change_condition_of_part(int index_of_part);//change condition of part by index on the opposite
+	
+	pair<int, int> get_position_of_part(int index_of_part);//return position of part by index
+	void set_position_of_part(int index_of_part, pair<int, int>);//set position of part by index
+
+	int get_length();//return length of the ship
+
+	~Ship();//destructor of the ship
 };
