3cda78e1edaea4387e17d76b47a5b86ae0450c3b
Рустам <0ne_eyed@ukr.net>
2015-11-13 21:16:01 +0200

    Implementing Ship class

--- Battle_ship/Ship.cpp
+++ Battle_ship/Ship.cpp
@@ -1,82 +1,100 @@
 #include "Ship.h"
+#include <algorithm>
 
+Ship::Ship(initializer_list<Ship::part> list):
+	on_injure{nullptr},
+	on_kill{nullptr},
+	killed{false},
+	parts{list} {}
 
-Ship::Ship(initializer_list<pair<bool, pair<int, int>>> list)
-{
-	this->parts.insert(this->parts.end(), list.begin(), list.end());
-	this->condition = true;
+bool Ship::is_killed() {
+	return this->killed;
 }
 
-bool Ship::get_condition()
-{
-	return this->condition;
+void Ship::set_on_kill_handler(function<void(const vector<Ship::part>&)> on_kill) {
+	this->on_kill = on_kill;
 }
 
-void Ship::set_condition(bool condition)
-{
-	this->condition = condition;
+void Ship::set_on_injure_handler(function<void(const vector<Ship::part>&)> on_injure) {
+	this->on_injure = on_injure;
 }
 
-void Ship::change_condition()
-{
-	this->condition = this->condition ? false : true;
-}
-
-vector<pair<bool, pair<int, int>>> Ship::get_parts()
-{
+vector<Ship::part> Ship::get_parts() {
 	return this->parts;
 }
 
-void Ship::set_parts(vector<pair<bool, pair<int, int>>> parts)
-{
+void Ship::set_parts(vector<Ship::part> parts) {
 	this->parts = parts;
 }
 
-pair<bool, pair<int, int>> Ship::get_part(int index_of_part)
-{
-	return parts[index_of_part];
-}
-
-void Ship::set_part(int index_of_part, pair<bool, pair<int, int>> part)
-{
-	this->parts[index_of_part] = part;
-}
-
-void Ship::add_part(pair<bool, pair<int, int>> part)
-{
+void Ship::add_part(Ship::part part) {
 	this->parts.push_back(part);
 }
 
-bool Ship::get_condition_of_part(int index_of_part)
-{
-	return this->parts[index_of_part].first;
+void Ship::delete_part(Ship::part part) {
+	auto pos = find(this->parts.begin(), this->parts.end(), part);
+
+	if (pos != this->parts.end())
+		this->parts.erase(pos);
 }
 
-void Ship::set_condition_of_part(int index_of_part, bool condition)
-{
-	this->parts[index_of_part].first = condition;
+bool Ship::is_part_injured(Ship::pos pos_of_part) {
+	auto part = this->get_part(pos_of_part);
+
+	return part != this->parts.end() ? this->injured(*part) : false;
 }
 
-void Ship::change_condition_of_part(int index_of_part)
-{
-	this->parts[index_of_part].first = this->parts[index_of_part].first ? false : true;
+void Ship::injure_part(Ship::pos pos_of_part) {
+	auto part = this->get_part(pos_of_part);
+
+	if (part != this->parts.end() && !this->injured(*part)) {
+		this->injured(*part) = true;
+		if (this->on_injure)
+			on_injure(pos_of_part);
+		if (this->all_injured()) {
+			this->killed = true;
+			if (this->on_kill)
+				on_kill(this->parts);
+		}
+	}
 }
 
-pair<int, int> Ship::get_position_of_part(int index_of_part)
-{
-	return this->parts[index_of_part].second;
+void Ship::rotate(Ship::pos center_pos, bool clockwise) {
+	auto center = this->get_part(center_pos);
+
+	switch (this->orient()) {
+	case Orient::Horizontal:
+		for (auto i = this->parts.begin(); i != this->parts.end(); ++i) {
+			this->x(*i) = this->x(*center);
+			this->y(*i) += distance(i, center) * (clockwise ? -1 : 1);
+		}
+		break;
+	case Orient::Vertical:
+		for (auto i = this->parts.begin(); i != this->parts.end(); ++i) {
+			this->x(*i) += distance(i, center) * (clockwise ? 1 : -1);
+			this->y(*i) = this->y(*center);
+		}
+		break;
+	}
 }
 
-void Ship::set_position_of_part(int index_of_part, pair<int, int> position_of_part)
-{
-	this->parts[index_of_part].second = position_of_part;
-}
-
-int Ship::get_length()
-{
+int Ship::get_length() {
 	return this->parts.size();
 }
 
-Ship::~Ship()
-{
+Ship::~Ship() {}
+
+vector<Ship::part>::iterator Ship::get_part(Ship::pos pos) {
+	return find_if(this->parts.begin(), this->parts.end(), [&](const Ship::part& p) {return p.second == pos; });
+}
+
+Ship::Orient Ship::orient() {
+	return
+		this->parts.front().second.first - this->parts.back().second.first == 0 ?
+		Ship::Orient::Horizontal :
+		Ship::Orient::Vertical;
+}
+
+bool Ship::all_injured() {
+	return find_if_not(this->parts.begin(), this->parts.end(), [](Ship::part& p) {return p.first; }) == this->parts.end();
 }
