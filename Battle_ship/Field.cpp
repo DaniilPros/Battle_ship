ea51e8259a90024469faabd0dee4034942c3ff9a
Рустам <0ne_eyed@ukr.net>
2015-11-17 18:22:54 +0200

    Implemented Field::Ship

--- Battle_ship/Field.cpp
+++ Battle_ship/Field.cpp
@@ -1,147 +1,159 @@
 #include "Field.h"
 #include <algorithm>
-Field::Field()
+
+Field::Field ()
 {
 }
 
 
-Field::~Field()
+void Field::hit (const Point& p)
 {
 }
-//
-//void Field::Ship::set_on_kill_handler(function<void(const vector<Field::Ship::part>&)> on_kill) {
-//	this->on_kill = on_kill;
-//}
-//
-//void Field::Ship::set_on_injure_handler(function<void(const vector<Field::Ship::part>&)> on_injure) {
-//	this->on_injure = on_injure;
-//}
-//
-//vector<Field::Ship::part> Field::Ship::get_parts() {
-//	return this->parts;
-//}
-//
-//void Field::Ship::set_parts(vector<Field::Ship::part> parts) {
-//	this->parts = parts;
-//}
-//
-//void Field::Ship::add_part(Field::Ship::part part) {
-//	this->parts.push_back(part);
-//}
-//
-//void Field::Ship::delete_part(Field::Ship::part part) {
-//	auto pos = find(this->parts.begin(), this->parts.end(), part);
-//
-//	if (pos != this->parts.end())
-//		this->parts.erase(pos);
-//}
-//
-//bool Field::Ship::is_part_injured(Field::Ship::pos pos_of_part) {
-//	auto part = this->get_part(pos_of_part);
-//
-//	return part != this->parts.end() ? this->injured(*part) : false;
-//}
-//
-//void Field::Ship::injure_part(Field::Ship::pos pos_of_part) {
-//	auto part = this->get_part(pos_of_part);
-//
-//	if (part != this->parts.end() && !this->injured(*part)) {
-//		this->injured(*part) = true;
-//		if (this->on_injure)
-//			on_injure(pos_of_part);
-//		if (this->all_injured()) {
-//			this->killed = true;
-//			if (this->on_kill)
-//				on_kill(this->parts);
-//		}
-//	}
-//}
-//
-//void Field::Ship::rotate(Field::Ship::pos center_pos, bool clockwise) {
-//	auto center = this->get_part(center_pos);
-//
-//	switch (this->orient()) {
-//	case Orient::Horizontal:
-//		for (auto i = this->parts.begin(); i != this->parts.end(); ++i) {
-//			this->x(*i) = this->x(*center);
-//			this->y(*i) += distance(i, center) * (clockwise ? -1 : 1);
-//		}
-//		break;
-//	case Orient::Vertical:
-//		for (auto i = this->parts.begin(); i != this->parts.end(); ++i) {
-//			this->x(*i) += distance(i, center) * (clockwise ? 1 : -1);
-//			this->y(*i) = this->y(*center);
-//		}
-//		break;
-//	}
-//}
-//
-//int Field::Ship::get_length() {
-//	return this->parts.size();
-//}
-//
-//Field::Ship::~Ship() {}
-//
-//vector<Field::Ship::part>::iterator Field::Ship::get_part(Field::Ship::pos pos) {
-//	return find_if(this->parts.begin(), this->parts.end(), [&](const Field::Ship::part& p) {return p.second == pos; });
-//}
-//
-//Field::Ship::Orient Field::Ship::orient() {
-//	return
-//		this->parts.front().second.first - this->parts.back().second.first == 0 ?
-//		Field::Ship::Orient::Horizontal :
-//		Field::Ship::Orient::Vertical;
-//}
-//
-//bool Field::Ship::all_injured() {
-//	return find_if_not(this->parts.begin(), this->parts.end(), [](Field::Ship::part& p) {return p.first; }) == this->parts.end();
-//}
 
-Field::Ship::Ship (Point start_point, Point end_point){
+void Field::place_ship (const Ship& ship)
+{
+}
+
+void Field::move_ship_by (const Point& p)
+{
+}
+
+Field::~Field ()
+{
+}
+
+Field::Ship::Ship (Point start_point, Point end_point) : Ship()
+{
 	if (start_point.x == end_point.x)
-		for (size_t i = start_point.y; i <= end_point.y; i++)
-			this->parts.emplace_back(start_point.x, int(i));
+		for (auto i = start_point.y; i <= end_point.y; i++)
+			this->parts.emplace_back(false, Point{start_point.x, i});
 	else
-		for (size_t i = start_point.x; i <= end_point.x; i++)
-			this->parts.emplace_back(i, start_point.x);
+		for (auto i = start_point.x; i <= end_point.x; i++)
+			this->parts.emplace_back(false, Point{i, start_point.x});
 }
 
-Field::Ship::Ship (Parts& parts) : parts{parts}
+Field::Ship::Ship (const Parts& parts) :
+	on_injure{nullptr},
+	on_kill{nullptr},
+	killed{false},
+	parts{parts}
 {
 }
 
-bool Field::Ship::is_killed () {
+bool Field::Ship::is_killed ()
+{
 	return this->killed;
 }
 
-void Field::Ship::set_on_kill_handler (function<void(const Parts&)> on_kill) {
+void Field::Ship::set_on_kill_handler (std::function<void (const Parts&)> on_kill)
+{
 	this->on_kill = on_kill;
 }
 
-void Field::Ship::set_on_injure_handler (function<void(const Point&)> on_injure) {
+void Field::Ship::set_on_injure_handler (std::function<void (const Point&)> on_injure)
+{
 	this->on_injure = on_injure;
 }
 
-bool Field::Ship::is_part_injured (const Point& point) {
-	/*auto part = this->get_part(point);
-	
-	return part != this->parts.end() ? part->injured : false;*/
-	return false;
+bool Field::Ship::is_part_injured (const Point& point)
+{
+	auto part = this->get_part(point);
+
+	return part != this->parts.end() ? part->injured : false;
 }
 
-void Field::Ship::injure_part (const Point& point) {
+Field::Ship::Parts& Field::Ship::get_parts ()
+{
+	return this->parts;
 }
 
-void Field::Ship::rotate (Point center, bool clockwise) {
+void Field::Ship::set_parts (const Parts& p)
+{
+	this->parts = p;
 }
 
-Field::Ship::~Ship () {
-}
-//
-//Field::Ship::Parts::iterator& Field::Ship::get_part (const Point& point) {
-//	auto x = find_if(this->parts.begin(), this->parts.end(), [&](Part& p) {return p.point == point; });
-//	return x;
-//}
+void Field::Ship::injure_part (const Point& point)
+{
+	auto part = this->get_part(point);
 
-bool Field::Ship::all_injured () {
+	if (part != this->parts.end() && !part->injured)
+	{
+		part->injured = true;
+		this->on_injure(point);
+
+		if (this->all_injured())
+		{
+			this->killed = true;
+			this->on_kill(this->parts);
+		}
+	}
+}
+
+Field::Ship & Field::Ship::rotate (Point center_point, bool clockwise)
+{
+	auto center = this->get_part(center_point);
+
+	switch (this->orient())
+	{
+	case Orient::Horizontal:
+		for (auto i = this->parts.begin(); i != this->parts.end(); ++i)
+		{
+			i->point.x = center->point.x;
+			i->point.y += distance(i, center) * (clockwise ? -1 : 1);
+		}
+		break;
+	case Orient::Vertical:
+		for (auto i = this->parts.begin(); i != this->parts.end(); ++i)
+		{
+			i->point.y = center->point.y;
+			i->point.x += distance(i, center) * (clockwise ? 1 : -1);
+		}
+		break;
+	}
+	return *this;
+}
+
+void Field::Ship::shift (const Point& p)
+{
+	for (auto& part : this->parts)
+	{
+		part.point.x += p.x;
+		part.point.y += p.y;
+	}
+}
+
+Field::Ship::Parts::iterator Field::Ship::get_part (const Point& point)
+{
+	return
+			std::find_if(
+				this->parts.begin(),
+				this->parts.end(),
+
+				[&](Part& p)
+				{
+					return p.point == point;
+				}
+			);
+}
+
+bool Field::Ship::all_injured ()
+{
+	return
+			std::find_if_not(
+				this->parts.begin(),
+				this->parts.end(),
+
+				[&](const Part& p)
+				{
+					return p.injured;
+				}
+			) != this->parts.end();
+}
+
+Field::Ship::Orient Field::Ship::orient ()
+{
+	return
+			this->parts.front().point.x == this->parts.back().point.x ?
+				Orient::Horizontal :
+				Orient::Vertical;
 }
--- Battle_ship/Field.h
+++ Battle_ship/Field.h
@@ -1,54 +1,204 @@
 #pragma once
 #include <vector>
 #include <functional>
-using namespace std;
 
-
+// Model of a field in the game
 class Field
 {
-	struct Point {
+public:
+	// Point on the game field
+	struct Point
+	{
 		int x;
 		int y;
-		bool operator==(const Point& p) const { return x == p.x && y == p.y; }
-		//bool operator==(Point& p) const { return x == p.x && y == p.y; }
+
+		Point (int x, int y):x{x}, y{y}
+		{
+		}
+
+		bool operator== (const Point& p) const
+		{
+			return x == p.x && y == p.y;
+		}
 	};
 
-	class Ship {
+	// Model of a ship in the game
+	class Ship
+	{
 	public:
-		struct Part {
+		// Part of the ship
+		// describes where does it located on the field
+		// and its state (injured or not)
+		struct Part
+		{
+			Part (bool injured, const Point& point)
+				: injured{injured},
+				  point{point}
+			{
+			}
+
 			bool injured;
 			Point point;
 		};
-		using Parts = vector<Part>;
 
-		Ship(Point start_point, Point end_point);
-		explicit Ship(Parts& parts);
+		// Orient describes orientation
+		// of the ship relatively the field
+		// on which it located
+		enum class Orient
+		{
+			Horizontal,
+			Vertical
+		};
 
-		bool is_killed();
+		using Parts = std::vector<Part>;
+		
+		/*
+			Initialize the parts of the ship 
+			with all points between start_point and end_point.
 
-		void set_on_kill_handler(function<void(const Parts&)>  on_kill);
-		void set_on_injure_handler(function<void(const Point&)> on_injure);
+			@param start_point the first point of the ship
+			@param end_point the last point of the ship
 
-		bool is_part_injured(const Point& point);
-		void injure_part(const Point& point);
+		*/
+		Ship (Point start_point, Point end_point);
+		
+		/*
+			Initialize the parts of the ship with parts.
 
-		void rotate(Point center, bool clockwise);
+			Note: may be used as default constructor
 
-		size_t get_length() { return this->parts.size(); }
+			@param parts Parts of the ship
+		*/
+		Ship (const Parts& parts = {});
 
-		~Ship();
+		/*
+			Returns the killed field of the ship.
+
+			@return the killed field
+		*/
+		bool is_killed ();
+		/*
+			Sets on_kill event handler.
+			
+			on_kill event occurs after the last 
+			part of the ship has been injured.
+			Handler gets all parts of the killed ship.
+
+			@param on_kill Is event handler that called when the ship has been killed.
+		*/
+		void set_on_kill_handler (std::function<void  (const Parts&)> on_kill);
+		
+		/*
+			Sets on_injure event handler.
+
+			on_injure event occurs after the
+			part of the ship has been injured.
+			Handler gets part that was injured.
+
+			@param on_kill Is event handler that called when the ship has been injured.
+		*/
+		void set_on_injure_handler (std::function<void  (const Point&)> on_injure);
+
+		/*
+			Finds part of the ship with  point equals to point
+			and return the injured field of found part.
+			If part wasn't found it returns false.
+
+			@param point Is a point of the Field
+			@return injured field of the part of the ship
+		*/
+		bool is_part_injured (const Point& point);
+
+		/*
+			Sets injured field of the part of 
+			the ship at the point to true.
+			
+			It also may call on_injure() and on_kill() event handlers.
+			@param point Is a point of the Field where to injure the ship
+		*/
+		void injure_part (const Point& point);
+
+		/*
+			Returns parts of the ship
+			@return Parts of teh ship
+		*/
+		Parts& get_parts ();
+
+		/*
+			Sets parts of the ship to passed Parts
+
+			@param p Parts that will be setted
+		*/
+		void set_parts (const Parts& p);
+
+		/*
+			Rotates the ship around the point at the center.
+			The direction of rotating depends to the clockwise parameter.
+
+			@param center Point around which the ship will be rotated
+			@param clockwise Flag that says in which direction the ship should be rotated
+			@return Reference to itself.
+		*/
+		Ship & rotate (Point center, bool clockwise);
+
+		/*
+			Shifts the ship by p.x and p.y
+
+			@param p Is actuall offset which will be added to each part of the ship
+		*/
+		void shift (const Point& p);
+
+		/*
+			Returns length of the ship
+
+			@return Length of the ship
+		*/
+		size_t get_length ()
+		{
+			return this->parts.size();
+		}
+
 	private:
-		//Parts::iterator& get_part(const Point& point);
-		bool all_injured();
+		/*
+			Finds the part with the point equals to a passed point.
 
-		function<void(const Point&)> on_injure;
-		function<void(const Parts&)> on_kill;
+			@param point Point which will be found.
+			@return An iterator that points to the found part.
+		*/
+		Parts::iterator get_part (const Point& point);
 
+		/*
+			Looks through all parts and determine
+			are all parts are injured.
+
+			@return true if all parts are injured, false in other cases
+		*/
+		bool all_injured ();
+
+		/*
+			Determine orientation of the ship.
+
+			@return Orient object
+		*/
+		Orient orient ();
+
+		// Event handlers
+		std::function<void  (const Point&)> on_injure;
+		std::function<void  (const Parts&)> on_kill;
+		
 		bool killed;
 		Parts parts;
 	};
-public:
-	Field();
-	~Field();
-};
 
+	Field ();
+	~Field ();
+
+	void hit (const Point& p);
+	void place_ship (const Ship& ship);
+	void move_ship_by (const Point& p);
+
+private:
+	std::function<void(const Point&)> on_miss;
+	std::function<void(const Point&)> on_ship_injure;
+	std::function<void(const Ship::Parts&)> on_ship_kill;
+};
